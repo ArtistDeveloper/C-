f85ada86c7b4cb22e7a61559c26de12d0e9dfa18
ArtistDeveloper <40491724+ArtistDeveloper@users.noreply.github.com>
2019-03-08 12:28:03 +0900

    unity
    unity 공굴리기, 장애물사용

---
+++ Ball.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    float sttPoint;
+    bool shdPrntOver20 = true;
+    bool shdPrntOver30 = true;
+    SphereCollider myCollider;
+    
+    void Start()
+    {
+        //Rigidbody myRgdbody = GetComponent<Rigidbody>(); <Rigitbody> -> 컴포넌트타입 ball 컴포넌트에서 rigidtbody 컴포넌트를 읽어와 체크.
+        //Debug.Log("useGravirty? : " + myRgdbody.useGravity);
+        // 다른 문법 사용 Debug.Log("useGravirty? : "+GetComponent<Rigidbody>().useGravity);
+        //Debug.Log("start");
+        sttPoint = transform.position.z;
+        myCollider = GetComponent<SphereCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = transform.position.z - sttPoint;
+        myCollider.radius = myCollider.radius + 0.01f;
+
+        if (distance > 30)
+        {
+            if (shdPrntOver30)
+            {
+                Debug.Log("Over 30 : " + distance);
+                shdPrntOver30 = false;
+            }
+        }
+
+        else if (distance > 20)
+        {
+            if (shdPrntOver20)
+            {
+                Debug.Log("Over 20:" + distance);
+                shdPrntOver20 = false;
+            }
+
+        }
+    }
+}
---
+++ Obstacle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    float alpha = -0.1f;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        float newXPosition = transform.position.x + alpha;
+        transform.position = new Vector3(newXPosition, 3.88f, -15.22f);
+
+        if (newXPosition < -4.0f)
+        {
+            alpha = 0.1f;
+        }
+
+        else if(newXPosition > 4.0f)
+        {
+            alpha = -0.1f;
+        }
+    }
+}
